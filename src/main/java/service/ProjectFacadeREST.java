70d7535bdbe183f3983f64bde76484d0655d8430
Michael Wermeester <michael.wermeester@me.com>
2011-08-30 00:33:46 +0200

    Use HashMaps to create the JSON returned by findAll method in ProjectFacadeRest.

--- src/main/java/service/ProjectFacadeREST.java
+++ src/main/java/service/ProjectFacadeREST.java
@@ -5,7 +5,14 @@
 package service;
 
 import be.luckycode.projetawebservice.Project;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,6 +24,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  *
@@ -59,11 +69,53 @@ public class ProjectFacadeREST extends AbstractFacade<Project> {
         return super.find(id);
     }
 
-    @GET
+    /*@GET
     @Override
     @Produces("application/json")
     public List<Project> findAll() {
         return super.findAll();
+    }*/
+    
+    @GET
+    @Produces("application/json")
+    public String findAll2() {
+        List<Project> projects = super.findAll();
+
+        String retVal = "";
+        
+        ObjectMapper mapper = new ObjectMapper();
+        
+        List<Map> projectList = new ArrayList<Map>();
+                
+        for (Project p : projects) {
+            Map<String, Object> projectData = new HashMap<String, Object>();
+            Map<String, Object> userStruct = new HashMap<String, Object>();
+            //Map<String, String> nameStruct = new HashMap<String, String>();
+            userStruct.put("userId", p.getUserCreated().getUserId().toString());
+            userStruct.put("username", p.getUserCreated().getUsername());
+            projectData.put("userCreated", userStruct);
+            projectData.put("dateCreated", new SimpleDateFormat("yyyy-MM-dd'T'h:m:ssZ").format(p.getDateCreated()));
+            projectData.put("flagPublic", p.getFlagPublic());
+            projectData.put("endDate", new SimpleDateFormat("yyyy-MM-dd'T'h:m:ssZ").format(p.getEndDate()));
+            projectData.put("startDate", new SimpleDateFormat("yyyy-MM-dd'T'h:m:ssZ").format(p.getStartDate()));
+            projectData.put("projectDescription", p.getProjectDescription());
+            projectData.put("projectId", p.getProjectId().toString());
+            projectData.put("projectTitle", p.getProjectTitle());
+            
+            projectList.add(projectData);
+        }
+        
+        HashMap<String, Object> retProjects = new HashMap<String, Object>();
+        retProjects.put("project", projectList);
+        
+        try {
+            retVal = mapper.writeValueAsString(retProjects);
+        } catch (IOException ex) {
+            Logger.getLogger(ProjectFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return retVal;
+        
     }
 
     @GET
