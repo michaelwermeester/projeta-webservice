dd462ec3e6eabe561ef89755ba858dec73dbb66e
Michael Wermeester <michael.wermeester@me.com>
2011-09-20 00:44:22 +0200

    Create path users/username/{username} and return user as JSON.

--- src/main/java/service/UsersFacadeREST.java
+++ src/main/java/service/UsersFacadeREST.java
@@ -5,10 +5,18 @@
 package service;
 
 import be.luckycode.projetawebservice.Users;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -17,6 +25,7 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import org.codehaus.jackson.map.ObjectMapper;
 
 /**
  *
@@ -58,12 +67,100 @@ public class UsersFacadeREST extends AbstractFacade<Users> {
     public Users find(@PathParam("id") Integer id) {
         return super.find(id);
     }
-
+    
     @GET
+    @Path("username/{username}")
+    @Produces("application/json")
+    public String findByUsername(@PathParam("username") String username) {
+        
+        Query q = em.createNamedQuery("Users.findByUsername");
+        q.setParameter("username", username);
+        
+        List<Users> userList = new ArrayList<Users>();
+        userList = q.getResultList();
+               
+        if (userList.size() == 1) {
+            
+            Users u = userList.get(0);
+            
+            ObjectMapper mapper = new ObjectMapper();
+        
+            List<Map> userMap = new ArrayList<Map>();
+            
+            Map<String, Object> userData = generateUserJSON(u);
+            
+            userMap.add(userData);
+            
+            String retVal = "";
+            
+            HashMap<String, Object> retUsers = new HashMap<String, Object>();
+            retUsers.put("users", userMap);
+        
+            try {
+                retVal = mapper.writeValueAsString(retUsers);
+            } catch (IOException ex) {
+                Logger.getLogger(UsersFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+            return retVal;
+        } else {
+            return "";
+        }
+                    
+    }
+
+    /*@GET
     @Override
     @Produces("application/json")
     public List<Users> findAll() {
         return super.findAll();
+    }*/
+    
+    @GET
+    @Produces("application/json")
+    public String findAll2() {
+        List<Users> users = super.findAll();
+
+        String retVal = "";
+        
+        ObjectMapper mapper = new ObjectMapper();
+        
+        List<Map> userList = new ArrayList<Map>();
+                
+        for (Users u : users) {
+            
+            Map<String, Object> userData = generateUserJSON(u);
+            
+            userList.add(userData);
+        }
+        
+        HashMap<String, Object> retUsers = new HashMap<String, Object>();
+        retUsers.put("users", userList);
+        
+        try {
+            retVal = mapper.writeValueAsString(retUsers);
+        } catch (IOException ex) {
+            Logger.getLogger(UsersFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return retVal;
+    }
+
+    private Map<String, Object> generateUserJSON(Users u) {
+        
+        Map<String, Object> userData = new HashMap<String, Object>();
+        
+        userData.put("userId", u.getUserId().toString());
+        userData.put("username", u.getUsername());
+        
+        if (u.getFirstName() != null)
+            userData.put("firstName", u.getFirstName());
+        if (u.getLastName() != null)
+            userData.put("lastName", u.getLastName());
+        if (u.getEmailAddress() != null)
+            userData.put("emailAddress", u.getEmailAddress());
+        
+        return userData;
     }
 
     @GET
