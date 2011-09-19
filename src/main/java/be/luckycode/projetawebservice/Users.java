cd8dc1f76d8d1f695be3e80b109960390eafeefd
Michael Wermeester <michael.wermeester@me.com>
2011-09-19 20:06:44 +0200

    Update Users to reflect database changes.

--- src/main/java/be/luckycode/projetawebservice/Users.java
+++ src/main/java/be/luckycode/projetawebservice/Users.java
@@ -15,7 +15,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
@@ -25,8 +27,6 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-import javax.persistence.SequenceGenerator;
-
 /**
  *
  * @author michael
@@ -34,38 +34,20 @@ import javax.persistence.SequenceGenerator;
 @Entity
 @Table(name = "users")
 @XmlRootElement
-//@SequenceGenerator(name = "sequenceUsers", sequenceName = "users_user_id_seq", allocationSize = 1)
 @NamedQueries({
     @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
     @NamedQuery(name = "Users.findByUserId", query = "SELECT u FROM Users u WHERE u.userId = :userId"),
     @NamedQuery(name = "Users.findByUsername", query = "SELECT u FROM Users u WHERE u.username = :username"),
-    @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password")})
+    @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password"),
+    @NamedQuery(name = "Users.findByFirstName", query = "SELECT u FROM Users u WHERE u.firstName = :firstName"),
+    @NamedQuery(name = "Users.findByLastName", query = "SELECT u FROM Users u WHERE u.lastName = :lastName"),
+    @NamedQuery(name = "Users.findByEmailAddress", query = "SELECT u FROM Users u WHERE u.emailAddress = :emailAddress"),
+    @NamedQuery(name = "Users.findByAddress", query = "SELECT u FROM Users u WHERE u.address = :address"),
+    @NamedQuery(name = "Users.findByPhoneNumber", query = "SELECT u FROM Users u WHERE u.phoneNumber = :phoneNumber"),
+    @NamedQuery(name = "Users.findByJobTitle", query = "SELECT u FROM Users u WHERE u.jobTitle = :jobTitle")})
 public class Users implements Serializable {
-    @ManyToMany(mappedBy = "usersCollection")
-    private Collection<Usergroup> usergroupCollection;
-    @OneToMany(mappedBy = "userCreated")
-    private Collection<BugProgress> bugProgressCollection;
-    @OneToMany(mappedBy = "userCreated")
-    private Collection<ProjectProgress> projectProgressCollection;
-    @OneToMany(mappedBy = "userAssigned")
-    private Collection<Task> taskCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userCreated")
-    private Collection<Task> taskCollection1;
-    @OneToMany(mappedBy = "userId")
-    private Collection<Contact> contactCollection;
-    @OneToMany(mappedBy = "userCreated")
-    private Collection<TaskProgress> taskProgressCollection;
-    @OneToMany(mappedBy = "userAssigned")
-    private Collection<Bug> bugCollection;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userReported")
-    private Collection<Bug> bugCollection1;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userCreated")
-    private Collection<Comment> commentCollection;
-    @OneToMany(mappedBy = "userCreated")
-    private Collection<Project> projectCollection;
     private static final long serialVersionUID = 1L;
     @Id
-    //@GeneratedValue(generator = "sequenceUsers", strategy = GenerationType.SEQUENCE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @NotNull
@@ -81,8 +63,51 @@ public class Users implements Serializable {
     @Size(min = 1, max = 1024)
     @Column(name = "password")
     private String password;
+    @Size(max = 100)
+    @Column(name = "first_name")
+    private String firstName;
+    @Size(max = 100)
+    @Column(name = "last_name")
+    private String lastName;
+    @Size(max = 255)
+    @Column(name = "email_address")
+    private String emailAddress;
+    @Size(max = 255)
+    @Column(name = "address")
+    private String address;
+    @Size(max = 30)
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Size(max = 100)
+    @Column(name = "job_title")
+    private String jobTitle;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Usergroup> usergroupCollection;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Project> projectCollection;
     @ManyToMany(mappedBy = "usersCollection")
     private Collection<Role> roleCollection;
+    @JoinColumn(name = "language", referencedColumnName = "language_code")
+    @ManyToOne
+    private Language language;
+    @OneToMany(mappedBy = "userCreated")
+    private Collection<BugProgress> bugProgressCollection;
+    @OneToMany(mappedBy = "userCreated")
+    private Collection<ProjectProgress> projectProgressCollection;
+    @OneToMany(mappedBy = "userAssigned")
+    private Collection<Task> taskCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userCreated")
+    private Collection<Task> taskCollection1;
+    @OneToMany(mappedBy = "userCreated")
+    private Collection<Project> projectCollection1;
+    @OneToMany(mappedBy = "userCreated")
+    private Collection<TaskProgress> taskProgressCollection;
+    @OneToMany(mappedBy = "userAssigned")
+    private Collection<Bug> bugCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userReported")
+    private Collection<Bug> bugCollection1;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userCreated")
+    private Collection<Comment> commentCollection;
 
     public Users() {
     }
@@ -114,11 +139,7 @@ public class Users implements Serializable {
     }
 
     public String getPassword() {
-        
-        //return password;
-        
-        // Don't return the password (for security reasons)
-        return "";
+        return password;
     }
 
     public void setPassword(String password) throws NoSuchAlgorithmException {
@@ -139,38 +160,61 @@ public class Users implements Serializable {
         this.password = sb.toString(); 
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
     @XmlTransient
-    public Collection<Role> getRoleCollection() {
-        return roleCollection;
+    public Collection<Usergroup> getUsergroupCollection() {
+        return usergroupCollection;
     }
 
-    public void setRoleCollection(Collection<Role> roleCollection) {
-        this.roleCollection = roleCollection;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (userId != null ? userId.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Users)) {
-            return false;
-        }
-        Users other = (Users) object;
-        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "be.luckycode.projetawebservice.Users[ userId=" + userId + " ]";
+    public void setUsergroupCollection(Collection<Usergroup> usergroupCollection) {
+        this.usergroupCollection = usergroupCollection;
     }
 
     @XmlTransient
@@ -183,12 +227,20 @@ public class Users implements Serializable {
     }
 
     @XmlTransient
-    public Collection<Usergroup> getUsergroupCollection() {
-        return usergroupCollection;
+    public Collection<Role> getRoleCollection() {
+        return roleCollection;
     }
 
-    public void setUsergroupCollection(Collection<Usergroup> usergroupCollection) {
-        this.usergroupCollection = usergroupCollection;
+    public void setRoleCollection(Collection<Role> roleCollection) {
+        this.roleCollection = roleCollection;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
     }
 
     @XmlTransient
@@ -228,12 +280,12 @@ public class Users implements Serializable {
     }
 
     @XmlTransient
-    public Collection<Contact> getContactCollection() {
-        return contactCollection;
+    public Collection<Project> getProjectCollection1() {
+        return projectCollection1;
     }
 
-    public void setContactCollection(Collection<Contact> contactCollection) {
-        this.contactCollection = contactCollection;
+    public void setProjectCollection1(Collection<Project> projectCollection1) {
+        this.projectCollection1 = projectCollection1;
     }
 
     @XmlTransient
@@ -270,6 +322,31 @@ public class Users implements Serializable {
 
     public void setCommentCollection(Collection<Comment> commentCollection) {
         this.commentCollection = commentCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (userId != null ? userId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.userId == null && other.userId != null) || (this.userId != null && !this.userId.equals(other.userId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "be.luckycode.projetawebservice.Users[ userId=" + userId + " ]";
     }
     
 }
