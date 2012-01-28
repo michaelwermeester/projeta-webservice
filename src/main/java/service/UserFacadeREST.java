c1696fb21eb4f5a584811137f35b08d6cb139561
Michael Wermeester <michael.wermeester@me.com>
2012-01-28 20:44:50 +0100

    Create UserFacadeREST -> findByUsergroup.

--- src/main/java/service/UserFacadeREST.java
+++ src/main/java/service/UserFacadeREST.java
@@ -6,6 +6,7 @@ package service;
 
 import be.luckycode.projetawebservice.Role;
 import be.luckycode.projetawebservice.User;
+import be.luckycode.projetawebservice.Usergroup;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -468,5 +469,52 @@ public class UserFacadeREST extends AbstractFacade<User> {
             return null;
         }
     }
+    
+    
+    // return list of users by specifying usergroup id.
+    @GET
+    @Path("usergroup/{usergroupid}")
+    @Produces("application/json")
+    public String findByUsergroup(@PathParam("usergroupid") Integer usergroupid) {
+
+        String retVal = "";
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Query q;
+        q = em.createNamedQuery("Usergroup.findByUsergroupId");
+        q.setParameter("usergroupId", usergroupid);
+
+        List<Usergroup> usergroupList = new ArrayList<Usergroup>();
+        usergroupList = q.getResultList();
+
+        if (usergroupList.size() == 1) {
+
+            Collection<User> users = usergroupList.get(0).getUserCollection();
+
+            List<Map> userList = new ArrayList<Map>();
+
+            for (User u : users) {
+
+                Map<String, Object> userData = generateUserJSON(u);
+
+                userList.add(userData);
+            }
+
+            HashMap<String, Object> retUsers = new HashMap<String, Object>();
+            retUsers.put("users", userList);
+
+            try {
+                retVal = mapper.writeValueAsString(retUsers);
+            } catch (IOException ex) {
+                Logger.getLogger(UserFacadeREST.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return retVal;
+        } else {
+            return "";
+        }
+    }
+                    
     
 }
