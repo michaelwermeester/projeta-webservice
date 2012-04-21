1d6c22f6502b011c82211ccf0791f10cd1e27be7
Michael Wermeester <michael.wermeester@me.com>
2012-04-21 14:21:08 +0200

    Bug fixes.

--- src/main/java/be/luckycode/projetawebservice/Task.java
+++ src/main/java/be/luckycode/projetawebservice/Task.java
@@ -24,12 +24,12 @@ import javax.xml.bind.annotation.XmlTransient;
 @NamedQueries({
     @NamedQuery(name = "Task.findAll", query = "SELECT t FROM Task t"),
     // get root projects (projects which have no parent) && exclude personal tasks.
-    @NamedQuery(name = "Task.getParentTasks", query = "SELECT t FROM Task t WHERE t.parentTaskId IS NULL and t.isPersonal = false"),
+    @NamedQuery(name = "Task.getParentTasks", query = "SELECT t FROM Task t WHERE t.parentTaskId IS NULL and t.isPersonal = false and (t.deleted = false or t.deleted is null)"),
     // get root projects (projects which have no parent) which are personal
-    @NamedQuery(name = "Task.getPersonalParentTasks", query = "SELECT t FROM Task t WHERE t.parentTaskId IS NULL and t.isPersonal = true and t.userCreated.userId = ?1"),
+    @NamedQuery(name = "Task.getPersonalParentTasks", query = "SELECT t FROM Task t WHERE t.parentTaskId IS NULL and t.isPersonal = true and (t.deleted = false or t.deleted is null) and t.userCreated.userId = ?1"),
     // get child projects
     // get root tasks by project ID & exclude personal tasks.
-    @NamedQuery(name = "Task.getParentTasksByProjectId", query = "SELECT t FROM Task t WHERE t.parentTaskId IS NULL and t.isPersonal = false and t.projectId.projectId = :projectId"),
+    @NamedQuery(name = "Task.getParentTasksByProjectId", query = "SELECT t FROM Task t WHERE t.parentTaskId IS NULL and t.isPersonal = false and t.projectId.projectId = :projectId and (t.deleted = false or t.deleted is null)"),
     //
     @NamedQuery(name = "Task.getChildTasks", query = "SELECT t FROM Task t WHERE t.parentTaskId = ?1"),
     @NamedQuery(name = "Task.findByTaskId", query = "SELECT t FROM Task t WHERE t.taskId = :taskId")})
